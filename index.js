12d892b0ed7f15d7fe5a204b1f2f8551bc32726e
Arnav-03 <arnavarora0003@gmail.com>
2024-03-12 22:43:15 +0530

    Fds

--- index.js
+++ index.js
@@ -1,19 +1,12 @@
-const cors = require('cors')
+const { log } = require("console");
 const { createServer } = require("http");
 const { Server } = require("socket.io");
 
 try {
     const httpServer = createServer();
-     /*  const corsOptions = {
-        credentials: true,
-        origin: "https://peermeet.onrender.com",
-    }; */
-    
- 
-    const io = new Server(8000, {
+    const io = new Server(httpServer, {
         cors: true,
     });
-
 
     const nameToSocketIdMap = new Map();
     const socketIdToNameMap = new Map();
@@ -26,19 +19,19 @@ try {
             const userName = user;
             const userRoomId = roomId;
             console.log(userName);
-
+        
             // Corrected: Join the socket to the room before emitting events
             socket.join(userRoomId);
-
+        
             nameToSocketIdMap.set(userName, socket.id);
             socketIdToNameMap.set(socket.id, userName);
-
+        
             // Emit events after joining the room
             io.to(userRoomId).emit("user:joined", { username: userName, id: socket.id });
             io.to(socket.id).emit("room:join", { username: userName, roomId });
         });
-
-
+        
+  
         socket.on('user:call', ({ to, offer }) => {
             io.to(to).emit('incoming:call', { from: socket.id, offer });
         });
@@ -46,13 +39,13 @@ try {
         socket.on('call:accepted', ({ to, ans }) => {
             io.to(to).emit('call:accepted', { from: socket.id, ans });
         });
-
+        
         socket.on('peer:nego:needed', ({ to, offer }) => {
-            io.to(to).emit('peer:nego:needed', { from: socket.id, offer });
+            io.to(to).emit('peer:nego:needed', { from: socket.id, offer});
         });
 
-        socket.on('peer:nego:done', ({ to, ans }) => {
-            io.to(to).emit('peer:nego:final', { from: socket.id, ans });
+        socket.on('peer:nego:done', ({ to, ans}) => {
+            io.to(to).emit('peer:nego:final', { from: socket.id, ans});
         });
     });
 
